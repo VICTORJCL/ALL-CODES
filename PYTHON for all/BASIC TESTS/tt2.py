e64ca4d35b1afceddbdaf930d5591e671be2ff11
VICTOR J. C LAMEIRO <victorjcldev@outlook.com>
2025-01-20 15:00:38 -0300

    EU ALTEREI O ÚLTIMO COMMIT!

--- PYTHON for all/BASIC TESTS/tt2.py
+++ PYTHON for all/BASIC TESTS/tt2.py
@@ -1,9 +1,9 @@
 import os
 # Obtém o diretório do arquivo atual
 caminho_atual = os.path.dirname(os.path.abspath(__file__))
-# Define o diretório de trabalho como o diretório do arquivoo
+# Define o diretório de trabalho como o diretório do arquivo
 os.chdir(caminho_atual)
 print(caminho_atual)
 print(str(caminho_atual))
 print('aqui')
-print('aqu2')
+print(type(caminho_atual))
